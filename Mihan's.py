4b687a616956645512ec6ae686f773edeafa0a87
SayuriWitharana <166282097+SayuriWitharana@users.noreply.github.com>
2025-03-24 11:20:46 +0530

    Add files via upload

---
+++ Mihan's.py
@@ -0,0 +1,6 @@
+def greet_user(name):
+    return f"Hello, {name}! Welcome to Mihan-project."
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    print(greet_user(user_name))
